cd2278eba4ec68636198a150a109cf98bcd442a5
Neculai Robert <robert.neculai13@gmail.com>
2020-02-26 21:26:48 +0200

    Added lambda expression and getProductbyId, custom exceptions, Optionals

--- src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
+++ src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
@@ -1,6 +1,7 @@
 package org.fasttrackit.onlineshop.service;
 
 import org.fasttrackit.onlineshop.domain.Product;
+import org.fasttrackit.onlineshop.exception.ResourceNotFoundException;
 import org.fasttrackit.onlineshop.persistance.ProductRepository;
 import org.fasttrackit.onlineshop.transfer.SaveProductRequest;
 import org.slf4j.Logger;
@@ -8,26 +9,43 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class ProductService {
-    private static final Logger LOGGER= LoggerFactory.getLogger(ProductService.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProductService.class);
 
     // inversion of control ( IoC)
-    private final ProductRepository productRepository ;
+    private final ProductRepository productRepository;
+
     // dependancy injection  (from IoC container)
-@Autowired
+    @Autowired
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
-    public Product createProduct(SaveProductRequest request){
-    LOGGER.info("Creating Product {}",request);
-    Product product= new Product();
-    product.setName(request.getName());
-    product.setDescription(request.getDescription());
-    product.setPrice(request.getPrice());
-    product.setQuantity(request.getQuantity());
-    product.setImageUrl(request.getImageUrl());
-    return productRepository.save(product);
+    public Product createProduct(SaveProductRequest request) {
+        LOGGER.info("Creating Product {}", request);
+        Product product = new Product();
+        product.setName(request.getName());
+        product.setDescription(request.getDescription());
+        product.setPrice(request.getPrice());
+        product.setQuantity(request.getQuantity());
+        product.setImageUrl(request.getImageUrl());
+        return productRepository.save(product);
+    }
+
+    public Product getProduct(long id) {
+        LOGGER.info("Retrieving product {}", id);
+        return productRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Product " + id + " not found"));
+        // Lambda expressions
+
+        // optional usage explained
+        //Optional<Product> productOptional = productRepository.findById(id);
+        //if(productOptional.isPresent())
+        //    return productOptional.get();
+        //else
+        //    throw new ResourceNotFoundException("Product "+id+" not found");
+        //}
     }
 }
